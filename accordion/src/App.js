cb7949553dc9f49b3585e60e64d49afa44df67bc
Oleksii Morenets <oleksii.morenets@gmail.com>
2025-10-02 16:07:28 +0200

    Accordion v2 - only one card is open

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -25,30 +25,34 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((item, i) => (
         <AccordionItem
           key={item.title}
-          num={i + 1}
+          num={i}
           title={item.title}
           text={item.text}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
         />
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, text, curOpen, onOpen }) {
+  const isOpen = curOpen === num;
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
     <div className={`item ${isOpen && "open"}`} onClick={handleToggle}>
-      <p className="number">{num < 10 ? `0${num}` : num}</p>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
       {isOpen && <p className="content-box">{text}</p>}
