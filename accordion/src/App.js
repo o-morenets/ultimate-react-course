48e59b4a41a8d003534d21c32d5ce89ce507923b
Oleksii Morenets <oleksii.morenets@gmail.com>
2025-10-02 15:43:43 +0200

    Accordion v1

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 const faqs = [
@@ -18,11 +19,39 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((item, i) => (
+        <AccordionItem
+          key={item.title}
+          num={i + 1}
+          title={item.title}
+          text={item.text}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen && "open"}`} onClick={handleToggle}>
+      <p className="number">{num < 10 ? `0${num}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <p className="content-box">{text}</p>}
+    </div>
+  );
 }
